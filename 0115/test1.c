0a4fbcbffb06f79cc53a987c91126376f012f68f
mobaiyan <3053288903@qq.com>
2023-01-15 15:20:07 +0800

    test

--- 0115/test1.c
+++ 0115/test1.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
-int Max(int ch[20])
-{
-    int max = 0;
-    int i = 0;
-    for (i = 0; i < 20; i++)
-    {
-        if (ch[i + 1] > ch[i])
-            max = ch[i + 1];
-        else max = ch[i];
-    }
-    return max;
-}
-int main()
-{
-    int max = 0;
-    int ch[20];
-    int a = 0, b = 0, c = 0;
-    int tmp = 0;
-    printf("input times with its");
-    scanf("%d", &a);
-    for (tmp = 0; tmp < a; tmp++)
-    {
-        getchar();
-        scanf("%d", ch[tmp++]);
-    }
-    printf("%d", max);
-    return 0;
-}
+//int Max(int ch[20])
+//{
+//    int max = 0;
+//    int i = 0;
+//    for (i = 0; i < 20; i++)
+//    {
+//        if (ch[i + 1] > ch[i])
+//            max = ch[i + 1];
+//        else max = ch[i];
+//    }
+//    return max;
+//}
+//int main()
+//{
+//    int max = 0;
+//    int ch[20];
+//    int a = 0, b = 0, c = 0;
+//    int tmp = 0;
+//    printf("input times with its");
+//    scanf("%d", &a);
+//    for (tmp = 0; tmp < a; tmp++)
+//    {
+//        getchar();
+//        scanf("%d", ch[tmp++]);//数组[不能是变量 只能是常量]
+//    }
+//    printf("%d", max);
+//    return 0;
+//}
+//怎么才能写一个可以不限制输入数数量的比大小的程序呢？ 
+//数组的大小不能用变量
