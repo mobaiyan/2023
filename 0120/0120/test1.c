7af2c038f049cc6cd6b6523856ccdb32970b4ec0
mobaiyan <3053288903@qq.com>
2023-01-21 00:55:38 +0800

    我终于知道头文件有什么用了！！！

---
+++ 0120/0120/test1.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"add.h"
+//int main()
+//{
+//	
+//	printf("%d", printf("%d", printf("%d", 43)));
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("%d", Add(a, b));
+//	return 0;
+//}
+// 
+// 
+// 
+// 输入数据打印
+//struct stu
+//{
+//    int no_;
+//    float C_score;
+//    double math_score;
+//    double English_score;
+//
+//};
+//int main()
+//{
+//    struct stu s = {0};
+//    struct stu * ps = &s;
+//    scanf("%d;%f,%lf,%lf", &ps->no_, &ps->C_score, &ps->math_score, &ps->English_score);
+//    printf("The each subject score of No. %d is %.2f, %.2lf, %.2lf.", s.no_, s.C_score, s.math_score, s.English_score);
+//    return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
