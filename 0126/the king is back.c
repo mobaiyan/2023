27f0437c6454dfa75fdf8c14d219073fa79e3ded
mobaiyan <3053288903@qq.com>
2023-01-26 23:33:08 +0800

    出了点小变故 明天就开始学习

---
+++ 0126/the king is back.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int tmp = 0;
+    //王者归来！！！经历了数天的努力 虽然我的300G数据全部丢失
+    //但是！我成功的将manjaro(简化版arch)完美配置好
+    printf("屏黑了?双击666\n");
+    scanf("%d",&tmp);
+    if(tmp==666)
+    {
+        printf("芜湖！\n");        
+    }
+    return 0;
+}
