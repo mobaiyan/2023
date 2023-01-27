d01ad826999519dc12355674bcf71563d7b62543
mobaiyan <3053288903@qq.com>
2023-01-28 01:00:12 +0800

    1

---
+++ 0128/test1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//1月27日 我的manjaro又出问题了 所以又修了一天 哎.....
+int my_strlen(char * str)//arr代表的是数组的首个字符的地址
+                        //所以用个指针存放
+{
+    if(*str!='\0')//*解引用指针后就是数组元素本身了
+        return 1+ my_strlen(str+1);//这里相当于0+1+1+1
+        //函数递归三次 最后一次return 0 不再进行递归
+        //递归结束时的return回到了主函数内
+    else
+        return 0;
+}
+int main()
+{
+    char arr[]="hello";
+    printf("%d\n",my_strlen(arr));//这里接收递归完成后的值
+    return 0;
+}
