cf775495863fb1bab4b6f9f551f1b4445bd9be30
mobaiyan <115197762+mobaiyan@users.noreply.github.com                   [http]>
2023-02-01 17:25:03 +0800

    今日第二次提交

---
+++ 0201/实现冒泡排序.c
@@ -0,0 +1,10 @@
+//
+// Created by zack on 2023/2/1.
+//
+#include "stdio.h"
+
+int main()
+{
+
+    return 0;
+}
---
+++ 0201/输出二维数组.c
@@ -0,0 +1,22 @@
+//
+// Created by zack on 2023/2/1.
+//
+#include "stdio.h"
+
+int main()
+{
+    int i = 0;
+    int n = 0;
+
+    char ch[][5] = {"fuck","you","life"};
+    for(i = 0;i < 3;i++)
+    {
+        for(n = 0;n < 5;n++)
+        {
+//            printf("%c",ch[i][n]);
+            printf("&ch[%d][%d] = %p\n",i ,n, &ch[i][n]);
+        }
+       // printf("\n");
+    }
+    return 0;
+}
