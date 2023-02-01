f3b8123d16b68b79a7a0a9f58b7b125702fcecb5
mobaiyan <115197762+mobaiyan@users.noreply.github.com>
2023-02-02 00:25:26 +0800

    ?

--- .vscode/每日C语言练习/0122.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("if success,im luck enough");
-    int a = 0;
-    a++;
-    a+=1;
-    return 0;
-}
--- .vscode/每日C语言练习/test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a=0;
-    scanf("%d",&a);
-    if(a==2) printf("add很好\n");
-    else printf("测试\n");
-    return 0;
-}
