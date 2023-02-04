b58724adee9d17a4f268476317f742b32782d84a
mobaiyan <115197762+mobaiyan@users.noreply.github.com>
2023-02-04 16:48:07 +0800

    .

--- CODES/0203/game.h
+++ CODES/0203/game.h
@@ -1,3 +1,19 @@
 #include<stdio.h>
 
-void Chess_board();
+
+//符号的定义
+#define ROW 3
+#define COL 3
+
+
+
+//函数的声明
+
+//初始化棋盘
+void InitBoard(char board[ROW][COL],int row,int col);
+
+//打印棋盘
+void DisplayBoard(char board[ROW][COL],int row,int col);
+
+//玩家下棋
+void Playmove(char board[ROW][COL],int row,int col);
--- CODES/0203/test.c
+++ CODES/0203/test.c
@@ -1,11 +1,28 @@
-#include<stdio.h>
-#include "game.h"
+#include"game.h"
+#include"game.c"//vscode要引用源文件才能找到头文件
+
 void menu()
 {
     printf("************************\n");
     printf("*********1.play*********\n");
     printf("*********0.exit*********\n");
     printf("************************\n");
+}
+
+void game()
+{
+    //创建数组
+    char board[ROW][COL];
+    //初始化棋盘 添加空格
+    InitBoard(board,ROW,COL);
+    //打印棋盘
+    DisplayBoard(board,ROW,COL);
+
+    while(1)//开始走棋
+    {
+        //玩家下棋
+        Playmove(board,ROW,COL);
+    }
 }
 
 int main()
@@ -17,10 +34,15 @@ int main()
         scanf("%d",&input);
         switch (input)
         {
-            case1: game();
-            case0: break;
+            case 1: 
+                game();
+                break;
+            case 0:
+                printf("exit game");
+                break;
             default: 
                 printf("error choice");
+                break;
         }
     } while (input);
 
