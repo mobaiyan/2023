8b0bfcef8223408180eb7e331dcf253ac61a5ab3
mobaiyan <3053288903@qq.com>
2023-02-10 21:43:23 +0800

    1

---
+++ CODES/examine/test1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    /*求阶乘
+    int n = 3;
+    int i = 0;
+    int sum = 1;
+
+    for(i = 1;i <= n; i++)
+    {
+        sum *= i;
+    }
+    printf("%d", sum);*/
+
+
+    return 0;
+}
